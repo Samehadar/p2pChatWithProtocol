6a9e79e58f07f1dd9e212ed1f5e206c7e3992c73
dosofredriver <dosofredriver@gmail.com>
2016-12-22 23:07:07 +0600

    Migrate from BigInteger to Int

--- src/main/java/com/samehadar/program/blakley/BlakleySecret.kt
+++ src/main/java/com/samehadar/program/blakley/BlakleySecret.kt
@@ -7,8 +7,8 @@ import java.math.BigInteger
 import java.util.*
 
 
-data class SecretPoint(val secret: BigInteger, val y: BigInteger, val z: BigInteger)
-data class Secret(val a1: BigInteger, val a2: BigInteger, val a3: BigInteger, val b: BigInteger) {
+data class SecretPoint(val secret: Int, val y: Int, val z: Int)
+data class Secret(val a1: Int, val a2: Int, val a3: Int, val b: Int) {
     fun toArray(): DoubleArray {
         return doubleArrayOf(a1.toDouble(), a2.toDouble(), a3.toDouble())
     }
@@ -16,24 +16,36 @@ data class Secret(val a1: BigInteger, val a2: BigInteger, val a3: BigInteger, va
         return b.toDouble()
     }
 }
+data class ThreeSecrets(val secret1: Secret, val secret2: Secret, val secret3: Secret)
 
-//TODO: get rid of BigInteger?
 object BlakleySecret {
     val random = Random(300)
 
     @JvmStatic
     fun main(args: Array<String>) {
         //Example of usage
-        val p = BigInteger.probablePrime(8, random)
-        val secret = BigInteger.valueOf(322L)
+        println("Type an integer as secret:")
+        val secret = readLine()!!.toInt()
+
+        val splitSecret = splitSecret(secret)
+        val restoredSecret = restoreSecret(splitSecret)
+        println("The restored secret is: " + restoredSecret)
+    }
+
+    fun splitSecret(secret: Int): ThreeSecrets {
+        val p = BigInteger.probablePrime(8, random).toLong()
 
         val secretPoint = getSecretPoint(secret, p)
-        val secret1 = makeSecret(secretPoint, p = p)
-        val secret2 = makeSecret(secretPoint, p = p)
-        val secret3 = makeSecret(secretPoint, p = p)
 
-        val restoredSecret = solve(secret1, secret2, secret3)
-        println("Restored secret is: " + restoredSecret)
+        return ThreeSecrets(
+                makeSecret(secretPoint, p = p),
+                makeSecret(secretPoint, p = p),
+                makeSecret(secretPoint, p = p)
+        )
+    }
+
+    fun restoreSecret(threeSecrets: ThreeSecrets): Long {
+        return solve(threeSecrets.secret1, threeSecrets.secret2, threeSecrets.secret3)
     }
 
     private fun solve(secret1: Secret, secret2: Secret, secret3: Secret): Long {
@@ -52,34 +64,27 @@ object BlakleySecret {
         return Math.round(solution.getEntry(0))
     }
 
-    private fun getSecretPoint(secret: BigInteger, p: BigInteger): SecretPoint {
-        val y = genRndBigInt(p)
-        val z = genRndBigInt(p)
+    private fun getSecretPoint(secret: Int, p: Long): SecretPoint {
+        val y = genRndInt(p)
+        val z = genRndInt(p)
 
         return SecretPoint(secret, y,z)
     }
 
-    private fun makeSecret(secretPoint: SecretPoint, p: BigInteger): Secret {
+    private fun makeSecret(secretPoint: SecretPoint, p: Long): Secret {
         val (secret, y, z) = secretPoint
 
         //TODO: we can generalize algorithm to `n` secret parts
-        val a1 = genRndBigInt(p)
-        val a2 = genRndBigInt(p)
-        val a3 = genRndBigInt(p)
+        val a1 = genRndInt(p)
+        val a2 = genRndInt(p)
+        val a3 = genRndInt(p)
 
         val b = a1 * secret + a2 * y + a3 * z
 
         return Secret(a1, a2, a3, b)
     }
 
-    private fun genRndBigInt(p: BigInteger): BigInteger {
-        var result: BigInteger
-
-        do {
-            result = BigInteger(p.bitLength(), random)
-        } while (result.compareTo(p) > -1)
-
-        return result
+    private fun genRndInt(p: Long): Int {
+        return (random.nextInt() % p).toInt()
     }
-
 }
